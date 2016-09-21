777c483438848b5f3f7b04af103cc37e4ddc8b4a
Zachary Snell <zach.snell@parivedasolutions.com>
2016-09-20 21:30:15 -0700

    Episode 2

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -9,7 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var timer: Timer?
+    var currentTime = 0
+    
+    @IBOutlet weak var stopWatchDisplay: UILabel!
+    @IBOutlet weak var stopWatchToggleButton: UIButton!
+    
+    @IBAction func stopWatchToggleTouchUpInside(_ sender: AnyObject) {
+        if timer != nil {
+            stopWatchToggleButton.setTitle("Start", for: .normal)
+            timer?.invalidate()
+            timer = nil
+        } else {
+            stopWatchToggleButton.setTitle("Stop", for: .normal)
+            timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
+                self.currentTime += 1
+                let minutesPortion = String(format: "%02d", self.currentTime / 60 )
+                let secondsPortion = String(format: "%02d", self.currentTime % 60 )
+                self.stopWatchDisplay.text = "\(minutesPortion):\(secondsPortion)"
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
